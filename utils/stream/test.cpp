7dbb586b8dcc69a667f1215eb4d795a98acb5cc0
hardened-steel <keldgaden@gmail.com>
2021-05-04 20:56:51 +0300

    add new unit test for stream/items

--- utils/stream/test.cpp
+++ utils/stream/test.cpp
@@ -14,15 +14,18 @@ void TouchFile(const fs::path& path, const std::string& value)
 		std::ofstream(path) << value;
 }
 
-namespace {
+namespace std {
 
-	auto& operator << (std::ostream& stream, std::byte byte)
+	std::ostream& boost_test_print_type(std::ostream& stream, const std::byte& byte)
 	{
 		const char symbols[] = "0123456789ABCDEF";
 		auto value = std::to_integer<unsigned char>(byte);
 		return stream << symbols[(value & 0xF0) >> 4u] << symbols[(value & 0x0F) >> 0u];
 	}
 }
+
+using items_ifstream_iterator = typename utils::items<utils::stream::ifstream>::iterator;
+BOOST_TEST_DONT_PRINT_LOG_VALUE(items_ifstream_iterator)
 
 BOOST_AUTO_TEST_SUITE(stream)
 
@@ -42,8 +45,38 @@ BOOST_AUTO_TEST_CASE(items)
 	const fs::path path = "fstream-test.data";
 	TouchFile(path, "hello from file");
 	utils::items fchannel = utils::stream::ifstream(path);
+	auto tmp = fchannel.begin();
+	auto it = tmp;
+	BOOST_TEST(*it == std::byte('h')); ++it;
+	BOOST_TEST(*it == std::byte('e')); ++it;
+	BOOST_TEST(*it == std::byte('l')); ++it;
+	BOOST_TEST(*it == std::byte('l')); ++it;
+	BOOST_TEST(*it == std::byte('o')); ++it;
+
+	BOOST_TEST(*it == std::byte(' ')); ++it;
+
+	BOOST_TEST(*it == std::byte('f')); ++it;
+	BOOST_TEST(*it == std::byte('r')); ++it;
+	BOOST_TEST(*it == std::byte('o')); ++it;
+	BOOST_TEST(*it == std::byte('m')); ++it;
+
+	BOOST_TEST(*it == std::byte(' ')); ++it;
+
+	BOOST_TEST(*it == std::byte('f')); ++it;
+	BOOST_TEST(*it == std::byte('i')); ++it;
+	BOOST_TEST(*it == std::byte('l')); ++it;
+	BOOST_TEST(*it == std::byte('e')); ++it;
+	BOOST_CHECK_EQUAL(it, fchannel.end());
+}
+
+BOOST_AUTO_TEST_CASE(items2)
+{
+	using utils::operator""_bytes;
+	const fs::path path = "fstream-test.data";
+	TouchFile(path, "hello from file");
+	utils::items fchannel = utils::stream::ifstream(path);
 	const auto str = "hello from file"_bytes;
-	BOOST_TEST(std::equal(fchannel.begin(), fchannel.end(), str.begin(), str.end()));
+	//BOOST_TEST(std::equal(fchannel.begin(), fchannel.end(), str.begin(), str.end()));
 }
 
 BOOST_AUTO_TEST_SUITE_END()
